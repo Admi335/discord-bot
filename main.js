9b9ca7cee2170de84be3c780df5a8d82f5712844
Admi335 <rihaadam1@seznam.cz>
2021-12-30 03:23:56 +0100

    Ugraded the license to GPLv3

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 /* This program is a Discord bot made using discord.js, a Discord API implementation for node.js
  * Copyright (C) 2021 Adam Říha
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  * You can contact me, the creator of this program, via this email address: rihaadam1@seznam.cz
  */
--- src/music.js
+++ src/music.js
@@ -1,9 +1,20 @@
 /* This program is a Discord bot made using discord.js, a Discord API implementation for node.js
- *
- * This program is licensed under the GPLv2 license
  * Copyright (C) 2021 Adam Říha
  *
- * See the whole notice in /main.js; a copy of the GNU General Public License is located in the root folder of this project
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * You can contact me, the creator of this program, via this email address: rihaadam1@seznam.cz
  */
 
 const { MessageEmbed, Client } = require('discord.js');
