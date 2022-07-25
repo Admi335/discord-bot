ab7c413e19175efa47bce47fe2cefd99b861b013
Adam Říha <64468662+Admi335@users.noreply.github.com>
2022-07-26 00:10:59 +0200

    Add execute function

--- src/commands/np.js
+++ src/commands/np.js
@@ -3,5 +3,27 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('np')
-        .setDescription('Now playing')
+        .setDescription('Now playing'),
+    async execute(interaction, player) {
+        await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+
+        if (!queue || !queue.playing)
+            return interaction.followUp({ content: "❌ | No music is being played!" });
+
+        const progress = queue.createProgressBar();
+        const perc = queue.getPlayerTimestamp();
+
+        return interaction.followUp({
+            embeds: [{
+                title: "Now playing",
+                description: `🎶 | **${queue.current.title}**! (\`${perc.progress}%\`)`,
+                fields: [{
+                    name: "\u200b",
+                    value: progress
+                }],
+                color: 0xffffff
+            }]
+        });
+    }
 }
