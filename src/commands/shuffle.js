3d6b8096830a72d76d3577ee296b16b78048b390
Adam Říha <64468662+Admi335@users.noreply.github.com>
2022-07-26 00:18:09 +0200

    Add execute function

--- src/commands/shuffle.js
+++ src/commands/shuffle.js
@@ -3,5 +3,12 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('shuffle')
-        .setDescription('Shuffle songs in the current queue')
+        .setDescription('Shuffle songs in the current queue'),
+    async execute(interaction, player) {
+        await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+
+        const success = queue.shuffle();
+        return interaction.followUp({ content: success ? 'Queue shuffled!' : 'Could not shuffle the queue!' });
+    }
 }
