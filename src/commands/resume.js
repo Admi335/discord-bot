b1de235e5319faa0b08a03e17478931ed9c3eeb5
Adam Říha <64468662+Admi335@users.noreply.github.com>
2022-07-26 00:18:03 +0200

    Add execute function

--- src/commands/resume.js
+++ src/commands/resume.js
@@ -3,5 +3,15 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('resume')
-        .setDescription('Resume the current song')
+        .setDescription('Resume the current song'),
+    async execute(interaction, player) {
+        await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+
+        if (!queue || !queue.playing)
+            return interaction.followUp({ content: "❌ | No music is being played!" });
+
+        const paused = queue.setPaused(false);
+        return interaction.followUp({ content: !paused ? "❌ | Something went wrong!" : "▶ | Resumed!" });
+    }
 }
