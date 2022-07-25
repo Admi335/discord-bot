6de11e491cd0d1ac8acda2ff076edebe65c5e427
Adam Říha <64468662+Admi335@users.noreply.github.com>
2022-07-26 00:18:16 +0200

    Add execute function

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -3,5 +3,15 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('stop')
-        .setDescription('Stop the player')
+        .setDescription('Stop the player'),
+    async execute(interaction, player) {
+        await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+
+        if (!queue || !queue.playing)
+            return interaction.followUp({ content: "❌ | No music is being played!" });
+
+        queue.destroy();
+        return interaction.followUp({ content: "🛑 | Stopped the player!" });
+    }
 }
