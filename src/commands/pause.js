abbd91dac0d346b6dc3c5f30c525e460175dc806
Adam Říha <64468662+Admi335@users.noreply.github.com>
2022-07-26 00:17:53 +0200

    Add execute function

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -3,5 +3,15 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('pause')
-        .setDescription('Pause the current song')
+        .setDescription('Pause the current song'),
+    async execute(interaction, player) {
+        await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+
+        if (!queue || !queue.playing)
+            return interaction.followUp({ content: "❌ | No music is being played!" });
+
+        const paused = queue.setPaused(true);
+        return interaction.followUp({ content: paused ? "⏸ | Paused!" : "❌ | Something went wrong!" });
+    }
 }
