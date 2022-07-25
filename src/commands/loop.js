d58c1b113594c7d279c9b998ebf359b168e4bdbd
Adam Říha <64468662+Admi335@users.noreply.github.com>
2022-07-26 00:17:50 +0200

    Add execute function

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -12,5 +12,18 @@ module.exports = {
                 .addChoice('Off', QueueRepeatMode.OFF)
                 .addChoice('Track', QueueRepeatMode.TRACK)
                 .addChoice('Queue', QueueRepeatMode.QUEUE)
-                .addChoice('Autoplay', QueueRepeatMode.AUTOPLAY))
+                .addChoice('Autoplay', QueueRepeatMode.AUTOPLAY)),
+    async execute(interaction, player) {
+        await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+
+        if (!queue || !queue.playing)
+            return interaction.followUp({ content: "❌ | No music is being played!" });
+
+        const loopMode = interaction.options.get("mode").value;
+
+        const success = queue.setRepeatMode(loopMode);
+        const mode = loopMode === QueueRepeatMode.TRACK ? "🔂" : loopMode === QueueRepeatMode.QUEUE ? "🔁" : "▶";
+        return interaction.followUp({ content: success ? `${mode} | Updated loop mode!` : "❌ | Could not update loop mode!" });
+    }
 }
