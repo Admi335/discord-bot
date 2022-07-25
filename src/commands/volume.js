37f5d5d880268692580e0a29dba49541f7e18fbd
Adam Říha <64468662+Admi335@users.noreply.github.com>
2022-07-26 00:18:20 +0200

    Add execute function

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -7,5 +7,22 @@ module.exports = {
         .addIntegerOption(option =>
             option.setName('amount')
                 .setDescription('The volume amount to set (0-100)')
-                .setRequired(false))
+                .setRequired(false)),
+    async execute(interaction, player) {
+        await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+
+        if (!queue || !queue.playing)
+            return interaction.followUp({ content: "❌ | No music is being played!" });
+
+        const vol = interaction.options.get("amount");
+
+        if (!vol)
+            return interaction.followUp({ content: `🎧 | Current volume is **${queue.volume}**%!` });
+        if ((vol.value) < 0 || (vol.value) > 100)
+            return interaction.followUp({ content: "❌ | Volume range must be 0-100" });
+
+        const success = queue.setVolume(vol.value);
+        return interaction.followUp({ content: success ? `✅ | Volume set to **${vol.value}%**!` : "❌ | Something went wrong!" });
+    }
 }
