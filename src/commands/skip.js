9d5219bbfc61e001f4933ae7cda121d3bd80762d
Adam Říha <64468662+Admi335@users.noreply.github.com>
2022-07-26 00:18:13 +0200

    Add execute function

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -3,5 +3,15 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('skip')
-        .setDescription('Skips the current song')
+        .setDescription('Skips the current song'),
+    async execute(interaction, player) {
+        await interaction.deferReply();
+        const queue = player.getQueue(interaction.guildId);
+
+        if (!queue || !queue.playing)
+            return await interaction.followUp({ content: "❌ | No music is being played!" });
+
+        const skipped = queue.skip();
+        return interaction.followUp({ content: skipped ? `✅ | Skipped **${queue.current}**!` : "❌ | Something went wrong!" });
+    }
 }
