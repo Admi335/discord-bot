5257d46fcbe6ee27d87b13aa62225de9c000b802
Adam Říha <64468662+Admi335@users.noreply.github.com>
2023-01-15 21:14:38 +0100

    Optimize findSubstring method with RegExp

--- src/findSubstring.js
+++ src/findSubstring.js
@@ -1,32 +1,6 @@
 module.exports = str => {
-
-    function findSubstring() {
-        let quote = "\"";
-        
-        for (let i = 0; i < 2; i++) {
-            let subStrI = -1;
-
-            do {
-                subStrI = str.indexOf(quote, subStrI + 1);
-            } while (subStrI != -1 && str[subStrI - 1] == "\\");
-
-            if (subStrI != -1) {
-                let subStrEnd = subStrI;
-                
-                do {
-                    subStrEnd = str.indexOf(quote, subStrEnd + 1);
-                } while (subStrEnd != -1 && str[subStrEnd - 1] == "\\");
-                
-                if (subStrEnd != -1) {
-                    return str.substring(subStrI + 1, subStrEnd);
-                }
-            }
-
-            if (i == 0) quote = "'";
-            else        return undefined;
-        }
-    }
-
-    return findSubstring();
+    
+    let match = str.match(/(['"])((?:[^"\\]|\\.)*)(?=\1)/);
+    return match ? match[2].replace(/\\(.)/g, "$1") : undefined;
 
 }
